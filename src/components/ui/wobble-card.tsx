7603888ae5dee92517d1570e8e72e7c5098a3237
Salah-Zain <salahzain36@gmail.com>
2026-02-16 22:46:56 +0530

    fixed error

--- src/components/ui/wobble-card.tsx
+++ src/components/ui/wobble-card.tsx
@@ -72,7 +72,7 @@ const Noise = () => {
         <div
             className="absolute inset-0 w-full h-full scale-[1.2] transform opacity-10 [mask-image:radial-gradient(#fff,transparent,75%)]"
             style={{
-                backgroundImage: "url(/noise.webp)",
+                backgroundImage: "url(\"data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E\")",
                 backgroundSize: "30%",
             }}
         ></div>
